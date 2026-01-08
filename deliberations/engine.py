5eb34a51093a6f28a52f0bf93f0c5c43e66ceea4
occook2 <119069061+occook2@users.noreply.github.com>
2026-01-08 13:23:57 -0600

    Added line to make logs folder if it doesn't exist already. Avoids error if logs folder not created

--- deliberations/engine.py
+++ deliberations/engine.py
@@ -183,6 +183,8 @@ class GameEngine:
                 f"Next round players: {[active_player_ids]} (from engine.py)"
             )
 
+        os.makedirs(self.game_config.logs_dir, exist_ok=True)
+        
         output_path = os.path.join(
             self.game_config.logs_dir, f"gameplay_{timestamp}.json"
         )
