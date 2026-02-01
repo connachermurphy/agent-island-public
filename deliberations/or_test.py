bef2efea8e91831159e70317be989e42b94e7e2b
Connacher Murphy <connacher.murphy@gmail.com>
2026-02-01 17:39:26 -0500

    Fix circular import

--- deliberations/or_test.py
+++ deliberations/or_test.py
@@ -1,11 +1,13 @@
-# from openrouter import OpenRouter
+from openrouter import OpenRouter
 # import os
 
-# with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as client:
-#     response = client.chat.send(
-#         model="minimax/minimax-m2",
-#         messages=[{"role": "user", "content": "Explain quantum computing"}],
-#     )
+with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as client:
+    response = client.chat.send(
+        model="minimax/minimax-m2",
+        messages=[{"role": "user", "content": "Please introduce yourself."}],
+    )
+
+    print(response)
 
 
 # from anthropic import Anthropic
